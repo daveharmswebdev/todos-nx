4a5838b35b3a42ffc68daccf9700db6b57073d12
Dave Harms <daveharmswebdev@gmail.com>
2021-09-08 04:58:58 -0500

    added feature modules

---
+++ apps/todos/src/app/doers/doers.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DoersComponent } from './doers/doers.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [DoersComponent],
+})
+export class DoersModule {}
---
+++ apps/todos/src/app/todos/todos.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TodosComponent } from './todos/todos.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TodosComponent],
+})
+export class TodosModule {}
