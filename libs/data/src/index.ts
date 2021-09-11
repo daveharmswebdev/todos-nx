42d5c9279a165a7828696567224f38604a7c062a
Dave Harms <daveharmswebdev@gmail.com>
2021-09-11 14:52:07 -0500

    added address cva

--- libs/data/src/index.ts
+++ libs/data/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/ITodo';
 export * from './lib/ITodoStatus';
 export * from './lib/ITodoToCreate';
+export * from './lib/IPerson';
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -4,9 +4,16 @@ import { TodoDisplayCardComponent } from './todo-display-card/todo-display-card.
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { PersonComponent } from './person/person.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   declarations: [TodoDisplayCardComponent, PersonComponent],
   exports: [MaterialModule, TodoDisplayCardComponent, PersonComponent],
 })
