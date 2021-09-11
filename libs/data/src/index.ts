a9716e59de198257f79fe116fbe794ccf806a421
Dave Harms <daveharmswebdev@gmail.com>
2021-09-11 16:35:46 -0500

    refactored todo list with new view type

--- libs/data/src/index.ts
+++ libs/data/src/index.ts
@@ -1,4 +1,5 @@
+export * from './lib/IPerson';
 export * from './lib/ITodo';
 export * from './lib/ITodoStatus';
 export * from './lib/ITodoToCreate';
-export * from './lib/IPerson';
+export * from './lib/ITodoView';
--- libs/data/src/lib/ITodo.ts
+++ libs/data/src/lib/ITodo.ts
@@ -1,9 +1,10 @@
+import { IPerson } from '..';
 import { TodoStatus } from './ITodoStatus';
 
 export interface ITodo {
   id: string;
   name: string;
-  assignedTo: string;
+  assignedTo: IPerson;
   description: string;
   status: TodoStatus;
 }
